846ed15938554de4079d71a0627bcb180c0bfff3
Sergio Ruiz <sergiotto@outlook.com>
2020-05-18 18:12:53 +0200

    more options menu

---
+++ src/pages/property-list/property-list.constants.js
@@ -0,0 +1,108 @@
+export const roomOptions = [
+    {
+        id: '1',
+        name: '+1'
+    },
+    {
+        id: '2',
+        name: '+2'
+    },
+    {
+        id: '3',
+        name: '+3'
+    },
+    {
+        id: '4',
+        name: '+4'
+    },
+    {
+        id: '5',
+        name: '+5'
+    },
+
+];
+export const bathroomOptions = [
+    {
+        id: '1',
+        name: '+1'
+    },
+    {
+        id: '2',
+        name: '+2'
+    },
+    {
+        id: '3',
+        name: '+3'
+    },
+    {
+        id: '4',
+        name: '+4'
+    },
+    {
+        id: '5',
+        name: '+5'
+    },
+
+];
+export const minPriceOptions = [
+    {
+        id: '300',
+        name: '300€'
+    },
+    {
+        id: '600',
+        name: '600€'
+    },
+    {
+        id: '900',
+        name: '900€'
+    },
+    {
+        id: '1200',
+        name: '1200€'
+    },
+    {
+        id: '1600',
+        name: '1600€'
+    },
+    {
+        id: '2000',
+        name: '2000€'
+    },
+    {
+        id: '2500',
+        name: '2500€'
+    },
+
+];
+export const maxPriceOptions = [
+    {
+        id: '100000',
+        name: '100000€'
+    },
+    {
+        id: '1200000',
+        name: '1200000€'
+    },
+    {
+        id: '140000',
+        name: '140000€'
+    },
+    {
+        id: '180000',
+        name: '180000€'
+    },
+    {
+        id: '200000',
+        name: '200000€'
+    },
+    {
+        id: '300000',
+        name: '300000€'
+    },
+    {
+        id: '400000',
+        name: '400000€'
+    },
+
+];
--- src/pages/property-list/property-list.js
+++ src/pages/property-list/property-list.js
@@ -1,6 +1,7 @@
 import { getPropertiesList, getSalesTypeList, getProvincesList } from './property-list.api';
 import { addPropertyRows, setOptions } from './property-list.helpers';
 import { mapPropertyListFromApiToVM } from './property-list.mappers';
+import { roomOptions, bathroomOptions, minPriceOptions, maxPriceOptions } from './property-list.constants'
 
 getPropertiesList().then((propertiesList) => {
     const viewModelPropertyList = mapPropertyListFromApiToVM(propertiesList);
@@ -15,3 +16,7 @@ getSalesTypeList().then((salesTypeList) => {
 getProvincesList().then((provincesList) => {
     setOptions(provincesList, 'select-province', '¿Dónde?');
 });
+setOptions(roomOptions, 'select-room', '¿Habitaciones?');
+setOptions(bathroomOptions, 'select-bathroom', '¿Baños?');
+setOptions(minPriceOptions, 'select-min-price', '¿Precio mínimo?');
+setOptions(maxPriceOptions, 'select-max-price', '¿Precio máximo?');
