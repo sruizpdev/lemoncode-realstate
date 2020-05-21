3ccfc1c30fcc228d07cadc3296457b6e39b1b563
Sergio Ruiz <sergiotto@outlook.com>
2020-05-21 13:22:48 +0200

    mas validaciones

--- src/pages/upload-property/upload-property.js
+++ src/pages/upload-property/upload-property.js
@@ -5,8 +5,6 @@ import { insertProperty, getSaleTypes, getEquipList } from './upload-property.ap
 import { setCheckboxList, setOptionList, onAddFeature, onAddImage } from './upload-property.helpers';
 import { getProvincesList } from '../property-list/property-list.api';
 
-
-
 let newProperty = {
     id: '',
     title: '',
@@ -25,24 +23,86 @@ let newProperty = {
     mainFeatures: [],
     equipmentIds: [],
     images: []
-
-
 }
+onUpdateField('city', (event) => {
+    const value = event.target.value;
+    newProperty = {
+        ...newProperty,
+        city: value
+    };
+    formValidation.validateField('city', newProperty.city).then(result => {
+        onSetError('city', result);
+    });
+});
+onUpdateField('rooms', (event) => {
+    const value = event.target.value;
+    newProperty = {
+        ...newProperty,
+        rooms: value
+    };
 
-
+    formValidation.validateField('rooms', newProperty.rooms).then(result => {
+        onSetError('rooms', result);
+    });
+});
 onUpdateField('title', (event) => {
     const value = event.target.value;
     newProperty = {
         ...newProperty,
         title: value
     };
-    console.log(newProperty);
-
 
     formValidation.validateField('title', newProperty.title).then(result => {
         onSetError('title', result);
     });
 });
+onUpdateField('address', (event) => {
+    const value = event.target.value;
+    newProperty = {
+        ...newProperty,
+        address: value
+    };
+
+    formValidation.validateField('address', newProperty.address).then(result => {
+        onSetError('address', result);
+    });
+});
+onUpdateField('bathrooms', (event) => {
+    const value = event.target.value;
+    newProperty = {
+        ...newProperty,
+        bathrooms: value
+    };
+
+    formValidation.validateField('bathrooms', newProperty.bathrooms).then(result => {
+        onSetError('bathrooms', result);
+    });
+});
+onUpdateField('locationUrl', (event) => {
+    const value = event.target.value;
+    newProperty = {
+        ...newProperty,
+        locationUrl: value
+    };
+
+    formValidation.validateField('locationUrl', newProperty.locationUrl).then(result => {
+        onSetError('locationUrl', result);
+    });
+});
+
+
+onUpdateField('squareMeter', (event) => {
+    const value = event.target.value;
+    newProperty = {
+        ...newProperty,
+        squareMeter: value
+    };
+
+    formValidation.validateField('squareMeter', newProperty.squareMeter).then(result => {
+        onSetError('squareMeter', result);
+    });
+});
+
 onUpdateField('notes', (event) => {
     const value = event.target.value;
     newProperty = {
@@ -70,7 +130,7 @@ onUpdateField('phone', (event) => {
     const value = event.target.value;
     newProperty = {
         ...newProperty,
-        email: value
+        phone: value
     };
 
     formValidation.validateField('phone', newProperty.phone).then(result => {
@@ -89,27 +149,33 @@ onUpdateField('price', (event) => {
     });
 });
 onSubmitForm('save-button', () => {
-    console.log(newProperty);
+
 
     formValidation.validateForm(newProperty).then(result => {
         onSetFormErrors(result);
 
-        if (result.succeeded) {
-            insertProperty(newProperty).then(() => { history.push(routes.propertyList) });
-        }
+
+        insertProperty(newProperty).then(() => { history.push(routes.propertyList) });
+
     });
 
 
 });
-let newFeature = '';
-onUpdateField('newFeature', (event) => {
-    newFeature = event.target.value;
 
+
+let newF = '';
+let newArray = [];
+
+onUpdateField('newFeature', (event) => {
+    newF = event.target.value;
 });
 onSubmitForm('insert-feature-button', () => {
-    if (newFeature != '') {
-        onAddFeature(newFeature);
-    }
+    newArray.push(newF);
+    console.log(newArray);
+    onAddFeature(newF);
+    newProperty.mainFeatures = newArray;
+    console.log(newProperty);
+
 });
 
 getProvincesList().then(provinceList => {
--- src/pages/upload-property/upload-property.validation.js
+++ src/pages/upload-property/upload-property.validation.js
@@ -1,4 +1,6 @@
 import { Validators, createFormValidation } from '@lemoncode/fonk';
+import { arrayRequired } from '@lemoncode/fonk-array-required-validator';
+import { isUrl } from '@lemoncode/fonk-is-url-validator';
 
 const validationShema = {
     field: {
@@ -7,8 +9,17 @@ const validationShema = {
         email: [Validators.required, Validators.email],
         phone: [Validators.required],
         price: [Validators.required],
-
+        address: [Validators.required],
+        city: [Validators.required],
+        provinceId: [Validators.required],
+        squareMeter: [Validators.required],
+        rooms: [Validators.required],
+        bathrooms: [Validators.required],
+        locationUrl: [Validators.required, isUrl.validator],
+        saleTypeIds: [arrayRequired.validator],
+        equipmentIds: [arrayRequired.validator],
     }
 };
+
 
 export const formValidation = createFormValidation(validationShema);
